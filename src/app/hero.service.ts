879d5f6f020848cbdd68df836a002ab9324c4b61
popescus <viktorpopescu@yahoo.com>
2019-04-15 19:25:54 +0300

    finished tutorial

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -6,6 +6,10 @@ import {MessageService} from './message.service';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {catchError, map, tap} from 'rxjs/operators';
 
+const httpOptions = {
+    headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
+
 @Injectable({
     providedIn: 'root',
 })
@@ -47,9 +51,6 @@ export class HeroService {
     }
 
     updateHero(hero: Hero): Observable<any> {
-        const httpOptions = {
-            headers: new HttpHeaders({'Content-Type': 'application/json'})
-        };
         return this.http.put(this.heroesUrl, hero, httpOptions).pipe(
             tap(_ => this.log(`updated hero id=${hero.id}`)),
             catchError(this.handleError<any>('updateHero'))
@@ -57,12 +58,29 @@ export class HeroService {
     }
 
     addHero(hero: Hero): Observable<Hero> {
-        const httpOptions = {
-            headers: new HttpHeaders({'Content-Type': 'application/json'})
-        };
         return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
             tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
             catchError(this.handleError<Hero>('addHero'))
+        );
+    }
+
+    deleteHero(hero: Hero | number): Observable<Hero>{
+        const id = typeof hero === 'number' ? hero: hero.id;
+        const url = `${this.heroesUrl}/${id}`;
+
+        return this.http.delete<Hero>(url, httpOptions).pipe(
+            tap( _ => this.log(`deleted hero id=${id}`)),
+            catchError(this.handleError<Hero>('deleteHero'))
+        );
+    }
+
+    searchHeroes(term: string): Observable<Hero[]> {
+        if (!term.trim()){
+            return of([]);
+        }
+        return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+            tap(_ => this.log(`found heroes matching "${term}"`)),
+            catchError(this.handleError<Hero[]>('searchHeroes', []))
         );
     }
 
