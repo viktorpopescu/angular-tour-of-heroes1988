852abc770fc47197fcf69c2f5ec18ed72dbc8ab2
popescus <viktorpopescu@yahoo.com>
2019-04-17 20:34:22 +0300

    finished tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import { Hero } from "./hero";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    // template:`
+    //   <h1>{{title}}</h1>
+    //   <h2>My favorite hero is: {{myHero}}</h2>
+    //   `,
+//     template: `
+//         <h1>{{title}}</h1>
+//         <h2>My favorite hero is: {{myHero}}</h2>
+//         <p>Heroes:</p>
+//         <p *ngIf="heroes.length > 3">There are many heroes! {{34+13 + summs(10,22)}}</p>
+//         <ul>
+//             <li *ngFor="let hero of heroes; index as i">
+//                 {{ hero.name }} : {{hero.id}} : {{i}}
+//             </li>
+//         </ul>
+//         <label>Type something:
+//             <input #customerInput>{{customerInput.value}}
+//         </label>
+// <!--        <div>{{customerInput.value}}</div>-->
+//     `,
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Tour of Heroes';
+    // title = 'Tour of Heroz';
+    // myHero = 'Windstorm';
+    heroes = [
+       new Hero(1,'Windstorm'),
+       new Hero(13,'Bombasto'),
+       new Hero(15,'Magneta'),
+       new Hero(20,'Tornado')];
+    myHero = this.heroes[0].name;
+    title: string;
+
+    constructor() {
+        this.title = 'Tour of Heroes';
+    }
+
+    summs(a,b): number {return a+b;}
 }
+
